695139532d247a445546981b95532ecb154a627c
seungh0 <will.seungho@gmail.com>
2021-03-11 23:39:50 +0900

    Init Project

---
+++ homecafe-api/src/main/java/com/homecafe/controller/ApiResponse.java
@@ -0,0 +1,20 @@
+package com.homecafe.controller;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ApiResponse<T> {
+
+	public static final ApiResponse<String> OK = new ApiResponse<>("", "", "OK");
+
+	private final String errorCode;
+	private final String message;
+	private final T data;
+
+	public static <T> ApiResponse<T> of(T data) {
+		return new ApiResponse<>("", "", data);
+	}
+
+}
